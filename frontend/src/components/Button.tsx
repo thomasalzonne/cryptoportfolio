558bc0c6da442e6cbc7169e4026d4e1089f4542d
Thomas Alzonne <thomas.alzonne@gmail.com>
2025-12-28 02:00:21 +0100

    add more space between icon and text in button

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -10,7 +10,7 @@ export default function Button({ title, icon: Icon, onClick }: ButtonProps) {
   return (
     <button 
       onClick={onClick}
-      className="flex w-full items-center gap-2 px-4 py-2 rounded-lg text-gray-800 dark:text-white transition-colors duration-300 hover:bg-gray-200 dark:hover:bg-gray-900 "
+      className="flex w-full items-center gap-4 px-4 py-2 rounded-lg text-gray-800 dark:text-white transition-colors duration-300 hover:bg-gray-200 dark:hover:bg-gray-900 "
     >
       <Icon size={20} />
       <span>{title}</span>
